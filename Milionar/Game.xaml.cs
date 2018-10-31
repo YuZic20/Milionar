1b10414087619b83bb712a3a7642e2f8ba2c442f
Martin Hynek <martin.hyn@email.cz>
2018-10-31 12:36:17 +0100

    color und des

--- Milionar/Game.xaml.cs
+++ Milionar/Game.xaml.cs
@@ -20,9 +20,60 @@ namespace Milionar
     /// </summary>
     public partial class Game : Page
     {
+        List<Button> LOfButtons = new List<Button>();
+        int LastButtonPr = 0;
+        int LastButtonPrCo = 1;
+        SolidColorBrush DefalutBackColor = Brushes.DarkCyan;
+        SolidColorBrush DefalutActiveColor = Brushes.Orange;
         public Game()
         {
+            
             InitializeComponent();
+            LOfButtons.Add(Button1);
+            LOfButtons.Add(Button2);
+            LOfButtons.Add(Button3);
+            LOfButtons.Add(Button4);
+            LOfButtons[0].Background = DefalutBackColor;
+            LOfButtons[1].Background = DefalutBackColor;
+            LOfButtons[2].Background = DefalutBackColor;
+            LOfButtons[3].Background = DefalutBackColor;
         }
+        
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            ColorChangeAndStart(1);
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            ColorChangeAndStart(2);
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            ColorChangeAndStart(3);
+        }
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            ColorChangeAndStart(4);
+        }
+        private void ColorChangeAndStart (int number)
+        {
+            if (LastButtonPr == number)
+            {
+                LOfButtons[number-1].Background = DefalutBackColor;
+                LastButtonPrCo = 1;
+                //do stuff
+                LastButtonPr = 0;
+            }
+            else
+            {
+                LOfButtons[number - 1].Background = DefalutActiveColor;
+                LOfButtons[LastButtonPrCo - 1].Background = DefalutBackColor;
+                LastButtonPr = number;
+                LastButtonPrCo = number;
+            }
+        }
+        //https://opentdb.com/
     }
 }
